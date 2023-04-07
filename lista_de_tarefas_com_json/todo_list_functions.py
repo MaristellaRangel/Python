4509423a40f3e0a5c48b36653acadb412374ab6a
Maristella Rangel <119543838+MaristellaRangel@users.noreply.github.com>
2023-04-07 09:40:05 -0300

    Create todo_list_functions.py

---
+++ lista_de_tarefas_com_json/todo_list_functions.py
@@ -0,0 +1,40 @@
+def verify_task_in_list(task, list):
+    if task not in list:
+        return True
+    return False
+
+
+def add_task(task, list):
+    if verify_task_in_list(task, list):
+        list.append(task)
+    else:
+        print("Task is already in list!")
+
+
+def verify_length_zero(list):
+    if len(list) == 0:
+        return True
+    return False
+
+
+def list(list):
+    print(*list, sep='\n')
+
+
+def undo(todo_list, undo_list):
+    if verify_length_zero(todo_list):
+        print("Nothing to undo!")
+    else:
+        task_to_undo = todo_list[-1]
+        add_task(task_to_undo, undo_list)
+        todo_list.pop()
+
+
+def redo(todo_list, undo_list):
+    if verify_length_zero(undo_list):
+        print("Nothing to redo")
+    else:
+        task_to_redo = undo_list[-1]
+        add_task(task_to_redo, todo_list)
+        undo_list.pop()
+
