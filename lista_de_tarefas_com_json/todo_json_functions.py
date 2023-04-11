71fa84df8c62d727c53c83b1521ef82abce9f59c
Maristella Rangel <119543838+MaristellaRangel@users.noreply.github.com>
2023-04-10 21:56:43 -0300

    Update todo_json_functions.py

--- lista_de_tarefas_com_json/todo_json_functions.py
+++ lista_de_tarefas_com_json/todo_json_functions.py
@@ -1,24 +1,59 @@
-import json
+def verify_task_in_list(task, list):
+    if task not in list:
+        return True
+    return False
 
 
-def get_lists(f):
-    try:
-        with open(f, 'r', encoding='utf8') as file:
-            content = json.load(file)
-    except FileNotFoundError:
-        with open(f, 'w', encoding='utf8') as file:
-            content = {"todo" : [], "undo": []}
-            json.dump(content, file)
-    finally:
-        todo = content["todo"]
-        undo = content["undo"]
-        return todo, undo
+def add_task(task, list):
+    if verify_task_in_list(task, list):
+        task.upper()
+        task = task.strip()
+        list.append(task)
+    else:
+        print("Task is already in list!")
 
 
-def set_lists(f, undo_list, todo_list):
-    with open(f, 'w', encoding='utf8') as file:
-        content = {
-            "todo" : todo_list,
-            "undo" : undo_list
-        }
-        json.dump(content, file)
+def delete_task(*args):
+    task = get_task_to_delete().upper()
+    for list in args:
+        if task in list:
+            list.remove(task)
+            task_found = True
+            break
+        else:
+            task_found = False
+
+    if not task_found:
+        print("Task not found")
+
+
+def get_task_to_delete():
+    task = input('task: ')
+    return task
+
+def verify_length_zero(list):
+    if len(list) == 0:
+        return True
+    return False
+
+
+def list(list):
+    print(*list, sep='\n')
+
+
+def undo(todo_list, undo_list):
+    if verify_length_zero(todo_list):
+        print("Nothing to undo!")
+    else:
+        task_to_undo = todo_list[-1]
+        add_task(task_to_undo, undo_list)
+        todo_list.pop()
+
+
+def redo(todo_list, undo_list):
+    if verify_length_zero(undo_list):
+        print("Nothing to redo")
+    else:
+        task_to_redo = undo_list[-1]
+        add_task(task_to_redo, todo_list)
+        undo_list.pop()
