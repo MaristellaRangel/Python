de09dd9b9a9dd51b29d92cc037affac028e0f82a
Maristella Rangel <119543838+MaristellaRangel@users.noreply.github.com>
2023-04-10 21:07:00 -0300

    Update todo_json_functions.py

--- lista_de_tarefas_com_json/todo_json_functions.py
+++ lista_de_tarefas_com_json/todo_json_functions.py
@@ -1,16 +1,22 @@
 import json
 
 
-def get_lists():
-    with open("todo.json", 'r', encoding='utf8') as arquivo:
-        content = json.load(arquivo)
+def get_lists(f):
+    try:
+        with open(f, 'r', encoding='utf8') as file:
+            content = json.load(file)
+    except FileNotFoundError:
+        with open(f, 'w', encoding='utf8') as file:
+            content = {"todo" : [], "undo": []}
+            json.dump(content, file)
+    finally:
         todo = content["todo"]
         undo = content["undo"]
         return todo, undo
 
 
-def set_lists(undo_list, todo_list):
-    with open('todo.json', 'w', encoding='utf8') as arquivo:
+def set_lists(f, undo_list, todo_list):
+    with open(f, 'w', encoding='utf8') as arquivo:
         content = {
             "todo" : todo_list,
             "undo" : undo_list
